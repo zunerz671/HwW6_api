5c539c4bc1b2c83b297152a474cacdbad4ed9244
zunerz671 <zunerz671@gmail.com>
2025-09-22 17:25:18 +1000

    Added model validations and associations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,6 @@
 class Tweet < ApplicationRecord
+  validates :tweet_text, presence: true
+  validates :user_id, presence: true
+
+  belongs_to :username
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :user_email, presence: true
+
+  has_many :tweets
 end
